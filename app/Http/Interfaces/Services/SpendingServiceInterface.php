5a0a0da8f7678efe12e47e2f316f2ee44b678f87
lucasandr3 <lucasvieiraandrade58@gmail.com>
2021-12-03 14:03:54 -0300

    implementado as rotas e metodos para spending limit

---
+++ app/Http/Interfaces/Services/SpendingServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Interfaces\Services;
+
+interface RevenueServiceInterface
+{
+    public function allRevenues();
+
+    public function getRevenue(int $revenue);
+
+    public function getInstallments(int $revenue);
+
+    public function newRevenue(array $resquest);
+}
